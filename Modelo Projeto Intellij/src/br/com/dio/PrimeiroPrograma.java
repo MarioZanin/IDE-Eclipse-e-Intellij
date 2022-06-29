d8ce505c2a42f29ae16d39e98e6989f38055c74b
MarioZanin <84603609+MarioZanin@users.noreply.github.com>
2022-06-29 15:42:01 -0300

    Add files via upload
    Modelo de programa Intellij

---
+++ Modelo Projeto Intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.Model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /* 2 int a = 5;
+        int b = 3;
+        System.out.println("HELLO WORD !!!  " + (a+b));*/
+        /* 1 System.out.println("HELLO WORD!!!");*/
+    }
+}
